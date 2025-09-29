7ed3a02250d64d47478aad0f906a96bb74f84739
Dominic <famdominic1218@gmail.com>
2025-09-29 11:09:34 +0800

    created a model call shopping

---
+++ app/src/main/java/com/dominic/shoppinglist/data/model/Shopping.kt
@@ -0,0 +1,9 @@
+package com.dominic.shoppinglist.data.model
+
+class Shopping (
+    val id: Int? = null,
+    val name: String,
+    val desc: String,
+    val price: Double,
+    val quantity: Int = 0
+)
