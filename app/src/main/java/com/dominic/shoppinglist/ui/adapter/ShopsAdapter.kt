453f26e65b4f5ef2d7fb68a523febbebee3b8977
Dominic <famdominic1218@gmail.com>
2025-10-01 11:45:54 +0800

    done creating the adapter

---
+++ app/src/main/java/com/dominic/shoppinglist/ui/adapter/ShopsAdapter.kt
@@ -0,0 +1,43 @@
+package com.dominic.shoppinglist.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.dominic.shoppinglist.data.model.Shop
+import com.dominic.shoppinglist.databinding.LayoutShopItemBinding
+
+class ShopsAdapter(
+    private var shops: List<Shop>,
+    private val onPress: (Shop) -> Unit
+): RecyclerView.Adapter<ShopsAdapter.ShopViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = LayoutShopItemBinding.inflate(inflater,parent, false)
+        return ShopViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ShopViewHolder, position: Int) {
+        val shop = shops[position]
+
+
+        holder.binding.run {
+            tvName.text = shop.name
+            tvDesc.text = shop.desc
+
+            cvShop.setOnClickListener {
+                onPress(shop)
+            }
+        }
+    }
+
+    override fun getItemCount() = shops.size
+
+    fun setShops(shops: List<Shop>) {
+        this.shops = shops
+        notifyDataSetChanged()
+    }
+
+    class ShopViewHolder(
+        val binding: LayoutShopItemBinding
+    ): RecyclerView.ViewHolder(binding.root)
+}
