b4174de34259a9992803a96826ce0f3842b4616a
Dominic <famdominic1218@gmail.com>
2025-10-04 21:13:33 +0800

    redo the add because its not adding

---
+++ app/src/main/java/com/dominic/shoppinglist/ui/manage/AddShopFragment.kt
@@ -0,0 +1,27 @@
+package com.dominic.shoppinglist.ui.manage
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import com.dominic.shoppinglist.R
+import com.dominic.shoppinglist.data.model.Shop
+
+class AddShopFragment : BaseManageFragment() {
+    override val viewModel: AddShopViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.run {
+            mbSubmit.text = getString(R.string.add)
+            mbSubmit.setOnClickListener {
+                viewModel.submit(
+                    Shop(
+                        name = etName.text.toString(),
+                        desc = etDesc.text.toString(),
+                        quantity = etQuantity.text.toString().toIntOrNull() ?: 0
+                    )
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/dominic/shoppinglist/ui/manage/AddShopViewModel.kt
@@ -0,0 +1,23 @@
+package com.dominic.shoppinglist.ui.manage
+
+import androidx.lifecycle.viewModelScope
+import com.dominic.shoppinglist.data.model.Shop
+import kotlinx.coroutines.launch
+
+class AddShopViewModel : BaseManageViewModel() {
+
+    override fun submit(shop: Shop) {
+        try {
+            require(shop.name.isNotBlank()) { "NO_NAME" }
+            require(shop.desc.isNotBlank()) { "NO_DESCRIPTION" }
+
+            repo.add(shop)
+
+            viewModelScope.launch {
+                _finish.emit(Unit)
+            }
+        } catch (e: Exception) {
+            viewModelScope.launch { _error.emit(e.message.toString()) }
+        }
+    }
+}
