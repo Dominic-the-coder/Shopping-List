c329b2a87ec030353876c42936dbe9885d836ca6
Dominic <famdominic1218@gmail.com>
2025-10-06 09:24:39 +0800

    recreate home

---
+++ app/src/main/java/com/dominic/shoppinglist/ui/home/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.dominic.shoppinglist.ui.home
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.dominic.shoppinglist.databinding.FragmentHomeBinding
+import com.dominic.shoppinglist.R
+
+class HomeFragment : BaseHomeFragment() {
+
+    override val viewModel: HomeViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.fabAdd.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToAddShopFragment()
+            findNavController().navigate(action)
+        }
+        binding.tvEmpty.text = getString(R.string.home_empty)
+    }
+}
