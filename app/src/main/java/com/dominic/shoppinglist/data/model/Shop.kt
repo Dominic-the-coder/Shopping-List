cbb946c6cdc52aea93e864a5ef8cfb75fcc9a055
Dominic <famdominic1218@gmail.com>
2025-10-06 15:14:03 +0800

    added staus

--- app/src/main/java/com/dominic/shoppinglist/data/model/Shop.kt
+++ app/src/main/java/com/dominic/shoppinglist/data/model/Shop.kt
@@ -1,8 +1,11 @@
 package com.dominic.shoppinglist.data.model
 
+import com.dominic.shoppinglist.data.enums.Status
+
 data class Shop (
     val name: String,
     val desc: String,
-    val quantity: Int? = 0,
+    val quantity: Int = 0,
+    val status: Status = Status.NOTINCART,
     val id: Int? = null,
 )
