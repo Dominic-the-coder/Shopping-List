1d74f1c1d01da501b0f2131ec82e204bd651f765
Dominic <famdominic1218@gmail.com>
2025-10-07 18:43:01 +0800

    make a constant file to store the hardcoded code

---
+++ app/src/main/java/com/dominic/shoppinglist/data/utils/Constant.kt
@@ -0,0 +1,7 @@
+package com.dominic.shoppinglist.data.utils
+
+object Constant {
+    const val MANAGE_SHOP = "manage_shop"
+    const val MANAGE_EDIT_SHOP = "manage_edit_shop"
+    const val SORTING_DIALOG = "SortingDialog"
+}
