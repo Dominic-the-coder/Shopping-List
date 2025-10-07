51ffb51a19ddc5d3b640ca05a12d44cb08a2d44f
Dominic <famdominic1218@gmail.com>
2025-10-07 18:13:39 +0800

    implement complete page and bottom nav

--- app/src/main/java/com/dominic/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/dominic/shoppinglist/MainActivity.kt
@@ -1,20 +1,56 @@
 package com.dominic.shoppinglist
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.dominic.shoppinglist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHost.findNavController()
+        binding.navView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, fragment, _ ->
+            when (fragment.id) {
+                R.id.homeFragment,
+                R.id.completeShopFragment -> showBotNav()
+
+                else -> hideBotNav()
+            }
+        }
+    }
+
+    fun showBotNav() {
+        binding.navView.animate().translationY(0f).setDuration(500)
+            .withStartAction { binding.navView.visibility = View.VISIBLE }
+    }
+
+    fun hideBotNav() {
+        binding.navView.animate().translationY(binding.navView.height.toFloat())
+            .setDuration(500)
+            .withEndAction { binding.navView.visibility = View.GONE }
     }
 }
