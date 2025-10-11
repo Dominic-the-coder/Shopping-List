ed98df156089dba1704120c9a30a6dc5e0d2fd47
Dominic <famdominic1218@gmail.com>
2025-10-11 18:18:55 +0800

    added category and change desc to notes

--- app/src/main/java/com/dominic/shoppinglist/data/model/Shop.kt
+++ app/src/main/java/com/dominic/shoppinglist/data/model/Shop.kt
@@ -4,8 +4,9 @@ import com.dominic.shoppinglist.data.enums.Status
 
 data class Shop (
     val name: String,
-    val desc: String,
+    val notes: String,
     val quantity: Int = 0,
+    val category: String,
     val status: Status = Status.NOTINCART,
     val id: Int? = null,
 )
